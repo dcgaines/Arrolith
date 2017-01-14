8c9345a1e30ed98b14fb492f44d0e122a9e96d99
dcgaines <dcgaines@mtu.edu>
2017-01-14 11:29:47 -0500

    Initial commit, blank project with arcadia implemented

---
+++ src/Test.java
@@ -0,0 +1,22 @@
+import java.awt.Graphics2D;
+import java.awt.Image;
+
+import arcadia.Game;
+import arcadia.Input;
+import arcadia.Sound;
+
+public class Test extends Game{
+
+	@Override
+	public void tick( Graphics2D graphics, Input input, Sound sound ) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Image cover( ) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
