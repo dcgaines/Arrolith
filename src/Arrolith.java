ece7fe9b77de2066070d41026fba13d79fcfe542
tleichte <tleichte@mtu.edu>
2017-02-04 17:38:15 -0500

    Added initial tick calculations and worked with graphics

--- src/Arrolith.java
+++ src/Arrolith.java
@@ -1,6 +1,8 @@
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.io.File;
+import java.io.FileNotFoundException;
+
 import javax.imageio.ImageIO;
 import arcadia.Arcadia;
 import arcadia.Button;
@@ -8,18 +10,56 @@ import arcadia.Game;
 import arcadia.Input;
 import arcadia.Sound;
 
+import java.util.Scanner;
+import java.util.prefs.Preferences;
+
 public class Arrolith extends Game {
 	//private enum STATES { STARTUP, MENU, INGAME, PAUSED; }
 	private final byte STARTUP = 0;
 	private final byte MENU = 1;
 	private final byte INGAME = 2;
 	private final byte PAUSED = 3;
+	private final byte PLAY = 0;
+	private final byte TWOPLAYERS = 1;
+	private final byte THREEPLAYERS = 2;
+	private final byte FOURPLAYERS = 3;
+	private final byte SOUND = 4;
+	private final byte QUIT = 5;
 	private byte currState;
+	private byte menuState;
+	private byte turnPhase;
+	private byte pauseState;
+	private boolean sound;
+	private File prefs;
+	Keys keys;
+	private int scrWidth;
+	private int scrHeight;
 	
 	
 	public Arrolith()
 	{
 		currState = STARTUP;
+		menuState = PLAY;
+		pauseState = PLAY;
+		sound = true;
+		keys = new Keys();
+		prefs = new File("prefs.txt");
+		try {
+			Scanner scan = new Scanner(prefs);
+			while (scan.hasNext())
+			{
+				String tmp = scan.nextLine();
+				if (tmp.contains("[SOUND]"))
+					sound = (scan.nextLine().contains("true"));
+				else if (tmp.contains("[GRAPHICS]"))
+					scrWidth = scan.nextInt();
+			}
+			if (scrWidth == 0) scrWidth = 1024;
+			scrHeight = (int) (scrWidth * (9.0/16));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		System.out.println(scrWidth);
 	}
 	
 	
@@ -28,6 +68,9 @@ public class Arrolith extends Game {
 	public void tick(Graphics2D graphics, Input input, Sound sound) {
 		
 		/*    		CALCULATIONS			 */
+		
+		
+		keys.checkKeys(input);
 		switch (currState)
 		{
 		case STARTUP:
@@ -43,6 +86,8 @@ public class Arrolith extends Game {
 			
 			break;
 		}
+		keys.setBuffers();
+		
 		
 		/*  			DRAWING			   */
 		switch (currState)
@@ -73,8 +118,6 @@ public class Arrolith extends Game {
 	}
 
 	public static void main(String[] args) {
-		
 		Arcadia.display(new Arcadia(new Game[] { new Arrolith() }));
-
 	}
 }
--- src/Graphics.java
+++ src/Graphics.java
@@ -2,6 +2,7 @@ import java.awt.Graphics2D;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import arcadia.Arcadia;
@@ -10,11 +11,11 @@ import arcadia.Game;
 import arcadia.Input;
 import arcadia.Sound;
 
-
 public class Graphics {
 	
 	static final int DIM = 22;
 	Image tileStrip;
+	Image sprites;
 	
 	public Graphics()
 	{
@@ -25,8 +26,7 @@ public class Graphics {
 		}
 	}
 	
-	
-	public void drawMap(Graphics2D g) throws IOException
+	public void drawMap(Graphics2D g, ArrayList<Player> players) throws IOException
 	{
 		Map map = new Map(2);
 		Tile tile;
@@ -34,8 +34,26 @@ public class Graphics {
 			for (int j = 0; j < 87; j++)
 			{
 				tile = map.getTile(i, j);
-				g.drawImage(tileStrip, i * DIM, j * DIM, (i + 1) * DIM, (j + 1) * DIM, tile.getType() * 32, 0, (tile.getType() + 1) * 32, (int) 32, null);
+				g.drawImage(tileStrip, i * DIM, j * DIM, (i + 1) * DIM, (j + 1) * DIM, tile.getType() * 32, 0, (tile.getType() + 1) * 32, 32, null);
 			}	
+		drawPlayers(players);
 	}
-	
+	private void drawPlayers(Graphics2D g, ArrayList<Player> players)
+	{
+		for (int i = 0; i < players.size(); i++)
+		{
+			//TODO	replace player with necessary methods
+			g.drawImage(sprites, 
+					players.get(i).getX() * DIM, players.get(i).getX() * DIM, 
+					(players.get(i).getX() + 1) * DIM, (players.get(i).getX() + 1) * DIM,
+					players.get(i).getType() * 32, 0,  (players.get(i).getType() + 1) * 32, 32, null);
+		}
+	}
+	public void drawHud(Graphics2D g, ArrayList<Player> players)
+	{
+		int posX, posY = 0;
+		for (int i = 0; i < players.size(); i++)
+			
+	}
 }
+
