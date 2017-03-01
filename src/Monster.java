52ba32e380336a9abcb371e4f2ee470602ec3862
dcgaines <dcgaines@mtu.edu>
2017-02-28 22:01:54 -0500

    Added a monster class that receives initial stats from its owner

---
+++ src/Monster.java
@@ -0,0 +1,58 @@
+
+public class Monster {
+
+	// Base damages, subject to balancing
+	static final double baseMeleeDmg = 1.0;
+	static final double baseRangeDmg = .7;
+	static final double baseMagicDmg = .8;
+
+	// Health
+	protected double maxHealth;
+	protected double health;
+	protected int actionPoints;
+	
+	protected Player owner;
+
+	// POINTS
+	protected int perseverance;
+	protected int observation;
+	protected int intellect;
+	protected int negotiation;
+	protected int tact;
+	protected int strength;
+
+	protected double meleeDmg;
+	protected double rangeDmg;
+	protected double magicDmg;
+
+	// Location on the map
+	protected int xCoord;
+	protected int yCoord;
+
+	public Monster( Player p ) {
+		owner = p;
+		updateStats( );
+	}
+
+	// Only call when first instantiated
+	public void updateStats( ) {
+		perseverance = owner.perseverance;
+		observation = owner.observation;
+		intellect = owner.intellect;
+		negotiation = owner.negotiation;
+		tact = owner.tact;
+		strength = owner.strength;
+		
+		xCoord = owner.xCoord;
+		yCoord = owner.yCoord;
+		
+		actionPoints = 2 + ( tact / 3 );
+		maxHealth = 20 + ( perseverance * 2 );
+		health = maxHealth;
+
+		meleeDmg = Player.baseMeleeDmg * ( 1 + ( strength - 1 ) / 10. );
+		rangeDmg = Player.baseRangeDmg * ( 1 + ( observation - 1 ) / 10. );
+		magicDmg = Player.baseMagicDmg * ( 1 + ( intellect - 1 ) / 10. );
+
+	}
+}
