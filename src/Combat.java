94b639c502931797bb4e49303c574449c170ad55
dcgaines <dcgaines@mtu.edu>
2017-04-19 22:11:01 -0400

    Only strong attacks have a crit chance now

--- src/Combat.java
+++ src/Combat.java
@@ -27,13 +27,13 @@ public class Combat {
 	boolean drawAction;
 	ArrayList<Action> actions;
 
-	public Combat(Player p, Monster m) {
+	public Combat( Player p, Monster m ) {
 		player = p;
-		actions = p.getActions();
+		actions = p.getActions( );
 
-		actionQueue = new ArrayList<Action>(2);
-		if (m == null)
-			monster = new Monster(0, 0, 1);
+		actionQueue = new ArrayList<Action>( 2 );
+		if ( m == null )
+			monster = new Monster( 0, 0, 1 );
 		else
 			monster = m;
 		selChoice = FIGHT;
@@ -43,116 +43,123 @@ public class Combat {
 		tFrame = 0;
 	}
 
-	private void checkKeys(Keys keys, byte leftRight, byte lrPress, byte upDown, byte udPress) {
-		if (keys.isKeyPressed(leftRight))
+	private void checkKeys( Keys keys, byte leftRight, byte lrPress, byte upDown, byte udPress ) {
+		if ( keys.isKeyPressed( leftRight ) )
 			selChoice = lrPress;
-		else if (keys.isKeyPressed(upDown))
+		else if ( keys.isKeyPressed( upDown ) )
 			selChoice = udPress;
 	}
 
-	public int calculate(Keys keys) {
-		if (transition) {
-			if (playerTurn) {
-				if (actionIndex == actionQueue.size()) {
-					actionQueue.clear();
+	public int calculate( Keys keys ) {
+		if ( transition ) {
+			if ( playerTurn ) {
+				if ( actionIndex == actionQueue.size( ) ) {
+					actionQueue.clear( );
 					selChoice = -2;
 					playerTurn = false;
 					transition = false;
 					betweenTurn = true;
 					actionIndex = 0;
-					if (monster.getHealth() <= 0) {
-						player.resetAP();
-						return monster.getLevel();
+					if ( monster.getHealth( ) <= 0 ) {
+						player.resetAP( );
+						return monster.getLevel( );
+					}
+				} else {
+					if ( tFrame < 1 ) {
+						perform( actionQueue.get( actionIndex ), player, monster );
+						drawAction = true;
+					}
+					if ( tFrame > 60 ) {
+						tFrame = -1;
+						actionIndex++;
+						drawAction = false;
 					}
 				}
-				else {
-				if (tFrame < 1) {
-					perform(actionQueue.get(actionIndex), player, monster);
+				tFrame++;
+			} else { // monster's turn
+				if ( tFrame < 1 ) {
+					makeMonsterAction( );
+					perform( monsterAction, monster, player );
 					drawAction = true;
 				}
-				if (tFrame > 60) {
+				if ( tFrame > 60 ) {
 					tFrame = -1;
-					actionIndex++;
-					drawAction = false;
-				}
-				}
-				tFrame++;
-			}
-			else { //monster's turn 
-				if (tFrame < 1) {
-				makeMonsterAction();
-				perform(monsterAction, monster, player);
-				drawAction = true;
-				}
-				if (tFrame > 60) {
-					tFrame = -1;
-					player.resetAP();
+					player.resetAP( );
 					selChoice = FIGHT;
 					transition = false;
 					drawAction = false;
 					playerTurn = true;
-					if (player.getHealth() <= 0) {
-						player.resetAP();
-						double mod = 1 - (player.negotiation / 10.);
-						return (int)(-monster.getLevel( ) * 20 * mod);
+					if ( player.getHealth( ) <= 0 ) {
+						player.resetAP( );
+						double mod = 1 - ( player.negotiation / 10. );
+						return (int) ( -monster.getLevel( ) * 20 * mod );
 					}
 				}
 				tFrame++;
 			}
-		}
-		 else {
-			if (playerTurn) {
-				switch (selChoice) {
+		} else {
+			if ( playerTurn ) {
+				switch ( selChoice ) {
 				case FIGHT:
-					if (keys.isKeyPressed(keys.A)) selChoice = ATTACK1;
-					else checkKeys(keys, keys.RIGHT, DEFEND, keys.DOWN, HEAL);
+					if ( keys.isKeyPressed( keys.A ) )
+						selChoice = ATTACK1;
+					else
+						checkKeys( keys, keys.RIGHT, DEFEND, keys.DOWN, HEAL );
 					break;
 				case DEFEND:
-					if (keys.isKeyPressed(keys.A)) playerTurn(DEFEND);
-					else checkKeys(keys, keys.LEFT, FIGHT, keys.DOWN, ENDTURN);
+					if ( keys.isKeyPressed( keys.A ) )
+						playerTurn( DEFEND );
+					else
+						checkKeys( keys, keys.LEFT, FIGHT, keys.DOWN, ENDTURN );
 					break;
 				case HEAL:
-					if (keys.isKeyPressed(keys.A)) playerTurn(HEAL);
-					else checkKeys(keys, keys.RIGHT, ENDTURN, keys.UP, FIGHT);
+					if ( keys.isKeyPressed( keys.A ) )
+						playerTurn( HEAL );
+					else
+						checkKeys( keys, keys.RIGHT, ENDTURN, keys.UP, FIGHT );
 					break;
 				case ENDTURN:
-					if (keys.isKeyPressed(keys.A)) playerTurn(ENDTURN);
-					else checkKeys(keys, keys.LEFT, HEAL, keys.UP, DEFEND);
+					if ( keys.isKeyPressed( keys.A ) )
+						playerTurn( ENDTURN );
+					else
+						checkKeys( keys, keys.LEFT, HEAL, keys.UP, DEFEND );
 					break;
 				case ATTACK1:
-					if (keys.isKeyPressed(keys.A)) playerTurn(ATTACK1);
-					else if (keys.isKeyPressed(keys.B)) selChoice = FIGHT;
+					if ( keys.isKeyPressed( keys.A ) )
+						playerTurn( ATTACK1 );
+					else if ( keys.isKeyPressed( keys.B ) )
+						selChoice = FIGHT;
 					else
-						checkKeys(keys, keys.RIGHT, ATTACK2, keys.DOWN, ATTACK3);
+						checkKeys( keys, keys.RIGHT, ATTACK2, keys.DOWN, ATTACK3 );
 					break;
 				case ATTACK2:
-					if (keys.isKeyPressed(keys.A))
-						playerTurn(ATTACK2);
-					else if (keys.isKeyPressed(keys.B))
+					if ( keys.isKeyPressed( keys.A ) )
+						playerTurn( ATTACK2 );
+					else if ( keys.isKeyPressed( keys.B ) )
 						selChoice = FIGHT;
 					else
-						checkKeys(keys, keys.LEFT, ATTACK1, keys.DOWN, ATTACK4);
+						checkKeys( keys, keys.LEFT, ATTACK1, keys.DOWN, ATTACK4 );
 					break;
 				case ATTACK3:
-					if (keys.isKeyPressed(keys.A))
-						playerTurn(ATTACK3);
-					else if (keys.isKeyPressed(keys.B))
+					if ( keys.isKeyPressed( keys.A ) )
+						playerTurn( ATTACK3 );
+					else if ( keys.isKeyPressed( keys.B ) )
 						selChoice = FIGHT;
 					else
-						checkKeys(keys, keys.RIGHT, ATTACK4, keys.UP, ATTACK1);
+						checkKeys( keys, keys.RIGHT, ATTACK4, keys.UP, ATTACK1 );
 					break;
 				case ATTACK4:
-					if (keys.isKeyPressed(keys.A))
-						playerTurn(ATTACK4);
-					else if (keys.isKeyPressed(keys.B))
+					if ( keys.isKeyPressed( keys.A ) )
+						playerTurn( ATTACK4 );
+					else if ( keys.isKeyPressed( keys.B ) )
 						selChoice = FIGHT;
 					else
-					 	checkKeys(keys, keys.LEFT, ATTACK3, keys.UP, ATTACK2);
+						checkKeys( keys, keys.LEFT, ATTACK3, keys.UP, ATTACK2 );
 					break;
 				}
-			} else if (betweenTurn) {
+			} else if ( betweenTurn ) {
 				tFrame++;
-				if (tFrame >= 60) {
+				if ( tFrame >= 60 ) {
 					tFrame = 0;
 					betweenTurn = false;
 				}
@@ -160,109 +167,101 @@ public class Combat {
 				transition = true;
 				selChoice = -2;
 			}
-			if (!playerTurn && monster.getHealth( ) <= 0 ) {
-				player.resetAP();
+			if ( !playerTurn && monster.getHealth( ) <= 0 ) {
+				player.resetAP( );
 				return monster.getLevel( );
-			} 
+			}
 		}
-		
+
 		return 0;
 	}
 
-	public void draw(Graphics2D g, ArroGraphics graphics) {
-		graphics.drawCombatOptions(g, selChoice, player);
-		graphics.drawBattleHUD(g, player);
-		graphics.drawCombatMonster(g, monster, betweenTurn, tFrame);
-		if (drawAction)
-				graphics.drawSelAction(g, playerTurn? actionQueue.get(actionIndex) : monsterAction,
-						playerTurn, player, amount);
+	public void draw( Graphics2D g, ArroGraphics graphics ) {
+		graphics.drawCombatOptions( g, selChoice, player );
+		graphics.drawBattleHUD( g, player );
+		graphics.drawCombatMonster( g, monster, betweenTurn, tFrame );
+		if ( drawAction )
+			graphics.drawSelAction( g, playerTurn ? actionQueue.get( actionIndex ) : monsterAction, playerTurn, player,
+					amount );
 		// draw defend
 		// draw heal
 		// draw endturn
 	}
 
-	private void playerTurn(int act) {
+	private void playerTurn( int act ) {
 
-		if (act == 6) {
+		if ( act == 6 ) {
 			transition = true;
 			selChoice = -2;
 			actionIndex = 0;
-			System.out.println("Ended turn. " + player.getHealth());
+			System.out.println( "Ended turn. " + player.getHealth( ) );
 			return;
 		}
-		Action performed = player.getActions().get(act);
-		if (performed.getCost() > player.getAP()) {
+		Action performed = player.getActions( ).get( act );
+		if ( performed.getCost( ) > player.getAP( ) ) {
 			return;
 		} else {
-			
-			actionQueue.add(performed);
-			player.actionPoints -= performed.getCost();
-			if (player.getAP() <= 0) {
+
+			actionQueue.add( performed );
+			player.actionPoints -= performed.getCost( );
+			if ( player.getAP( ) <= 0 ) {
 				transition = true;
 				selChoice = -2;
 			}
 		}
 	}
 
-	private void makeMonsterAction() {
+	private void makeMonsterAction( ) {
 		int numActions;
-		if (monster.getHealth() < monster.getMaxHealth())
+		if ( monster.getHealth( ) < monster.getMaxHealth( ) )
 			numActions = 4;
 		else
 			numActions = 3;
 
-		int act = (int) (Math.random() * numActions);
-		monsterAction = monster.getActions().get(act);
+		int act = (int) ( Math.random( ) * numActions );
+		monsterAction = monster.getActions( ).get( act );
 	}
 
-	private void perform(Action a, Being p, Being o) {
+	private void perform( Action a, Being p, Being o ) {
 		// leave alone
-		switch (a.getActionType()) {
+		switch ( a.getActionType( ) ) {
 		case Action.ATTACK:
-			switch (a.getWeaponType()) {
+			switch ( a.getWeaponType( ) ) {
 			case Action.MELEE:
-				if (a.getAttackType() == Action.WEAK)
-					amount = randDmg(p.meleeDmg, p.tact);
-				else
-					amount = randDmg(2 * p.meleeDmg, p.tact);
+				amount = randDmg( p.meleeDmg, p.tact, a.getAttackType( ) == Action.STRONG );
 				break;
 			case Action.RANGED:
-				if (a.getAttackType() == Action.WEAK)
-					amount = randDmg(p.rangeDmg, p.tact);
-				else
-					amount = randDmg(p.rangeDmg * 2, p.tact);
+					amount = randDmg( p.rangeDmg, p.tact, a.getAttackType( ) == Action.STRONG );
 				break;
 			case Action.MAGIC:
-				if (a.getAttackType() == Action.WEAK)
-					amount = randDmg(p.magicDmg, p.tact);
-				else
-					amount = randDmg(p.magicDmg * 2, p.tact);
+					amount = randDmg( p.magicDmg, p.tact, a.getAttackType( ) == Action.STRONG );
 				break;
 			}
-			amount = o.subtractHealth(amount);
+			amount = o.subtractHealth( amount );
 			break;
 		case Action.HEAL:
 			amount = 400 + 20 * p.intellect;
-			if(p instanceof Player)
+			if ( p instanceof Player )
 				amount /= 3;
-			p.addHealth(amount);
+			p.addHealth( amount );
 			break;
 		case Action.DEFEND:
-			p.defend();
+			p.defend( );
 			break;
 		}
 	}
-	
-	private int randDmg(int damage, int tact){
+
+	private int randDmg( int damage, int tact, boolean strong ) {
 		double min = tact / 10. + .4;
-		double dmg = damage * ( (Math.random( ) * (1 - min) + min));
-		
-		double crit = Math.random( );
-		if (crit < tact / 30.){
-			dmg *= 1.5;
+		double dmg = damage * ( ( Math.random( ) * ( 1 - min ) + min ) );
+		if ( strong ) {
+			dmg *= 2;
+			double crit = Math.random( );
+			if ( crit < tact / 30. ) {
+				dmg *= 1.5;
+			}
 		}
-			
-		
+
 		return (int) dmg;
 	}
 }
